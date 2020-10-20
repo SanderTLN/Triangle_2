05b21d926d626417c61aca665ac03492cc4400ee
opilane <opilane@HP-CZC6297B89>
2020-10-20 15:47:02 +0300

    Complete design and add some functions

--- Triangle_2/Form1.cs
+++ Triangle_2/Form1.cs
@@ -12,218 +12,230 @@ namespace Triangle_2
 {
     public partial class Form1 : Form
     {
+        Label txt1, txt2, txt3, txt4;
+        TextBox txtA, txtB, txtC, txtH, txtP, txtS;
+        CheckBox checkA, checkB, checkC, checkH, checkP, checkS, checkFP, checkFS;
+        Button btn1, btn2;
+        ListBox lbox;
+        PictureBox img;
         public Form1()
         {
             Width = 462;
             Height = 412;
-            Label txt1 = new Label();
+            txt1 = new Label();
             txt1.Text = "Введите данные";
             txt1.Size = new Size(132, 19);
             txt1.Location = new Point(13, 13);
             Controls.Add(txt1);
-            Label txt2 = new Label();
+
+            txt2 = new Label();
             txt2.Text = "Что найти?";
             txt2.Size = new Size(90, 19);
             txt2.Location = new Point(13, 246);
             Controls.Add(txt2);
-            Label txt3 = new Label();
+
+            txt3 = new Label();
             txt3.Text = "Решение";
             txt3.Size = new Size(70, 19);
             txt3.Location = new Point(214, 13);
             Controls.Add(txt3);
-            TextBox txtA = new TextBox();
+
+            txt4 = new Label();
+            txt4.Text = "Треугольник:";
+            txt4.Size = new Size(170, 19);
+            txt4.Location = new Point(140, 350);
+            Controls.Add(txt4);
+
+            txtA = new TextBox();
             txtA.Location = new Point(88, 39);
             txtA.Size = new Size(100, 20);
             Controls.Add(txtA);
-            TextBox txtB = new TextBox();
+            txtA.TextChanged += TxtA_TextChanged;
+
+            txtB = new TextBox();
             txtB.Location = new Point(88, 65);
             txtB.Size = new Size(100, 20);
             Controls.Add(txtB);
-            TextBox txtC = new TextBox();
+            txtB.TextChanged += TxtB_TextChanged;
+
+            txtC = new TextBox();
             txtC.Location = new Point(88, 91);
             txtC.Size = new Size(100, 20);
             Controls.Add(txtC);
-            TextBox txtH = new TextBox();
+            txtC.TextChanged += TxtC_TextChanged;
+
+            txtH = new TextBox();
             txtH.Location = new Point(88, 117);
             txtH.Size = new Size(100, 20);
             Controls.Add(txtH);
-            TextBox txtS = new TextBox();
+            txtH.TextChanged += TxtH_TextChanged;
+
+            txtS = new TextBox();
             txtS.Location = new Point(88, 143);
             txtS.Size = new Size(100, 20);
             Controls.Add(txtS);
-            TextBox txtP = new TextBox();
+            txtS.TextChanged += TxtS_TextChanged;
+
+            txtP = new TextBox();
             txtP.Location = new Point(88, 169);
             txtP.Size = new Size(100, 20);
             Controls.Add(txtP);
-            CheckBox checkA = new CheckBox();
+            txtP.TextChanged += TxtP_TextChanged;
+
+            checkA = new CheckBox();
             checkA.Text = "Сторона A";
             checkA.Size = new Size(78, 17);
             checkA.Location = new Point(13, 42);
             Controls.Add(checkA);
-            CheckBox checkB = new CheckBox();
+
+            checkB = new CheckBox();
             checkB.Text = "Сторона B";
             checkB.Size = new Size(78, 17);
             checkB.Location = new Point(13, 68);
             Controls.Add(checkB);
-            CheckBox checkC = new CheckBox();
+
+            checkC = new CheckBox();
             checkC.Text = "Сторона C";
             checkC.Size = new Size(78, 17);
             checkC.Location = new Point(13, 94);
             Controls.Add(checkC);
-            CheckBox checkH = new CheckBox();
+
+            checkH = new CheckBox();
             checkH.Text = "Высота";
             checkH.Size = new Size(78, 17);
             checkH.Location = new Point(13, 120);
             Controls.Add(checkH);
-            CheckBox checkS = new CheckBox();
+
+            checkS = new CheckBox();
             checkS.Text = "Площадь";
             checkS.Size = new Size(78, 17);
             checkS.Location = new Point(13, 146);
             Controls.Add(checkS);
-            CheckBox checkP = new CheckBox();
+
+            checkP = new CheckBox();
             checkP.Text = "Периметр";
             checkP.Size = new Size(78, 17);
             checkP.Location = new Point(13, 172);
             Controls.Add(checkP);
-            CheckBox checkFS = new CheckBox();
+
+            checkFS = new CheckBox();
             checkFS.Text = "Площадь";
             checkFS.Size = new Size(73, 17);
             checkFS.Location = new Point(13, 271);
             Controls.Add(checkFS);
-            CheckBox checkFP = new CheckBox();
+
+            checkFP = new CheckBox();
             checkFP.Text = "Периметр";
             checkFP.Size = new Size(77, 17);
             checkFP.Location = new Point(13, 294);
             Controls.Add(checkFP);
-            Button btn1 = new Button();
+
+            btn1 = new Button();
             btn1.Text = "Создать";
             btn1.Size = new Size(90, 45);
             btn1.Location = new Point(13, 195);
             Controls.Add(btn1);
-            Button btn2 = new Button();
+            btn1.Click += Btn1_Click;
+
+            btn2 = new Button();
             btn2.Text = "Найти";
             btn2.Size = new Size(90, 45);
             btn2.Location = new Point(13, 317);
             Controls.Add(btn2);
-            ListBox lbox = new ListBox();
+
+            lbox = new ListBox();
             lbox.Size = new Size(217, 238);
             lbox.Location = new Point(214 ,38);
             Controls.Add(lbox);
-            PictureBox img = new PictureBox();
+
+            img = new PictureBox();
             img.Size = new Size(91, 68);
             img.Location = new Point(340, 294);
             Controls.Add(img);
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void TxtH_TextChanged(object sender, EventArgs e)
         {
-
+            if (txtH.TextLength > 0)
+            {
+                checkH.Enabled = false;
+            }
+            else
+            {
+                checkH.Enabled = true;
+            }
         }
 
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        private void TxtC_TextChanged(object sender, EventArgs e)
+        {
+            if (txtC.TextLength > 0)
+            {
+                checkC.Enabled = false;
+            }
+            else
+            {
+                checkC.Enabled = true;
+            }
+        }
+
+        private void TxtB_TextChanged(object sender, EventArgs e)
+        {
+            if (txtB.TextLength > 0)
+            {
+                checkB.Enabled = false;
+            }
+            else
+            {
+                checkB.Enabled = true;
+            }
+        }
+
+        private void TxtP_TextChanged(object sender, EventArgs e)
+        {
+            if (txtP.TextLength > 0)
+            {
+                checkP.Enabled = false;
+                checkFP.Enabled = false;
+            }
+            else
+            {
+                checkP.Enabled = true;
+                checkFP.Enabled = true;
+            }
+        }
+
+        private void TxtS_TextChanged(object sender, EventArgs e)
+        {
+            if(txtS.TextLength > 0)
+            {
+                checkS.Enabled = false;
+                checkFS.Enabled = false;
+            }
+            else
+            {
+                checkS.Enabled = true;
+                checkFS.Enabled = true;
+            }
+        }
+
+        private void TxtA_TextChanged(object sender, EventArgs e)
+        {
+            if (txtA.TextLength > 0)
+            {
+                checkA.Enabled = false;
+            }
+            else
+            {
+                checkA.Enabled = true;
+            }
+        }
+
+        private void Btn1_Click(object sender, EventArgs e)
         {
             
         }
 
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox3_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void checkB_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void checkC_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void checkH_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void checkS_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void checkP_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtA_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtB_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtC_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtH_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtS_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtP_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btn1_Click(object sender, EventArgs e)
-        {
-            double a, b, c;
-            a = Convert.ToDouble(txtA.Text);
-            b = Convert.ToDouble(txtB.Text);
-            c = Convert.ToDouble(txtC.Text);
-        }
-
-        private void checkFS_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void checkFP_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btn2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox4_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
---
+++ Triangle_2/Triangle.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle_2
+{
+    class Triangle
+    {
+        public double a;
+        public double b;
+        public double c;
+        public double h;
+
+        public double GetSetA
+        {
+            get { return a; }
+            set { a = value; }
+        }
+
+        public double GetSetB
+        {
+            get { return b; }
+            set { b = value; }
+        }
+
+        public double GetSetC
+        {
+            get { return c; }
+            set { c = value; }
+        }
+
+        public double GetSetH
+        {
+            get { return h; }
+            set { h = value; }
+        }
+
+        public bool ExistTriangle
+        {
+            get
+            {
+                if ((a < b + c) && (b < a + c) && (c < a + b))
+                    return true;
+                else return false;
+            }
+        }
+
+        public string TriangleType
+        {
+            get
+            {
+                if (a == b && b == c && c == a)
+                {
+                    return "Равносторонний";
+                }
+                else if (b == c || a == b || c == a)
+                {
+                    return "Равнобедренный";
+                }
+                else return "Разносторонний";
+            }
+        }
+
+        public Triangle(double A, double B, double C)
+        {
+            a = A;
+            b = B;
+            c = C;
+            h = Height();
+        }
+
+        public Triangle(bool byHeight, double A, double H)
+        {
+            if (byHeight == true)
+            {
+                a = A;
+                h = H;
+                b = h;
+                c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(h, 2));
+            }
+            else
+            {
+                a = A;
+                h = H;
+                b = Math.Sqrt(Math.Pow(a / 2, 2) + Math.Pow(h, 2));
+                c = b;
+            }
+        }
+
+        public Triangle()
+        {
+            a = 0;
+            b = 0;
+            c = 0;
+        }
+
+        public string OutputA()
+        {
+            return Convert.ToString(a);
+        }
+
+        public string OutputB()
+        {
+            return Convert.ToString(b);
+        }
+
+        public string OutputC()
+        {
+            return Convert.ToString(c);
+        }
+
+        public double Perimeter()
+        {
+            double p = a + b + c;
+            return p;
+        }
+
+        public double HalfPerimeter()
+        {
+            return Perimeter() / 2;
+        }
+
+        public double Surface()
+        {
+            double p = HalfPerimeter();
+            double s = Math.Sqrt((p * (p - a) * (p - b) * (p - c)));
+            return s;
+        }
+
+        public double Height()
+        {
+            double p = HalfPerimeter();
+            double h = 2 * Math.Sqrt(p * (p - a) * (p - b) * (p - c)) / a;
+            return h;
+        }
+    }
+}
